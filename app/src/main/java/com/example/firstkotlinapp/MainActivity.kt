1640390252a6c1e55445650171e6db8688a72be1
Tal <tal.sitton04@gmail.com>
2022-09-06 16:20:17 +0300

    removed redundant lines

--- app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
@@ -106,7 +106,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        setSupportActionBar(findViewById(R.id.toolbar))
 
         jsonToList()
         resetToDefault()
