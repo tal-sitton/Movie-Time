f81db9476b3ac11b69e8674397f956c79d92e71e
tal-sitton <tal.sitton04@gmail.com>
2022-09-15 11:06:34 +0300

    about button now return to main screen when in about screen

--- app/src/main/java/com/example/firstkotlinapp/AboutActivity.kt
+++ app/src/main/java/com/example/firstkotlinapp/AboutActivity.kt
@@ -3,6 +3,7 @@ package com.example.firstkotlinapp
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
 
@@ -21,6 +22,22 @@ class AboutActivity : MyTemplateActivity() {
             val browserIntent =
                 Intent(Intent.ACTION_VIEW, Uri.parse("https://tal-sitton.github.io/Movie-Time/"))
             startActivity(browserIntent)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_reset -> {
+                MainActivity.resetToDefault()
+                MainActivity.filter()
+                recreate()
+                true
+            }
+            R.id.action_about -> {
+                onBackPressed()
+                true
+            }
+            else -> onOptionsItemSelected(item)
         }
     }
 }
