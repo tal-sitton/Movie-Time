d09d97a3591e8b26d7ecf928e14be1bb4d66dee0
tal-sitton <tal.sitton04@gmail.com>
2023-03-18 11:20:10 +0200

    redundant import

--- app/src/main/java/com/example/movietime/CinemaActivity.kt
+++ app/src/main/java/com/example/movietime/CinemaActivity.kt
@@ -11,7 +11,6 @@ import android.os.Looper
 import android.os.Message
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
