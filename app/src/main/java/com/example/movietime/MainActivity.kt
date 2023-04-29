fb9ba951e73869f48cfac26a1550fc2274f66e68
tal-sitton <tal.sitton04@gmail.com>
2023-04-29 18:39:13 +0300

    added loading animation when loading poster, and handle the exception when cant fetch poster when there is not connection to the internet

--- app/src/main/java/com/example/movietime/MainActivity.kt
+++ app/src/main/java/com/example/movietime/MainActivity.kt
@@ -255,7 +255,7 @@ class MainActivity : MyTemplateActivity() {
         }
 
         CoroutineScope(Dispatchers.Default).launch {
-            movie.generatePoster(findViewById(R.id.poster), this@MainActivity)
+            movie.showPoster(findViewById(R.id.poster), this@MainActivity)
         }
 
         val popUp = findViewById<ConstraintLayout>(R.id.infoPopup)
@@ -273,7 +273,7 @@ class MainActivity : MyTemplateActivity() {
         findViewById<ImageView>(R.id.poster).setImageDrawable(
             ContextCompat.getDrawable(
                 this,
-                R.drawable.baseline_image_24
+                R.drawable.image_placeholder
             )
         )
     }
