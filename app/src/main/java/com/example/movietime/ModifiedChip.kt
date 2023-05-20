77ad601da4a4255f431b185959ecfaf4186d8152
tal-sitton <tal.sitton04@gmail.com>
2023-05-20 11:30:41 +0300

    created custom view for Chip

---
+++ app/src/main/java/com/example/movietime/ModifiedChip.kt
@@ -0,0 +1,52 @@
+package com.example.movietime
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import java.lang.reflect.Method
+
+class ModifiedChip(
+    context: Context, attrs: AttributeSet? = null
+) : androidx.appcompat.widget.AppCompatTextView(context, attrs) {
+
+    var checked = false
+        set(value) {
+            field = value
+            if (value) {
+                setBackgroundResource(R.drawable.chip_selected)
+            } else {
+                setBackgroundResource(R.drawable.chip_unselected)
+            }
+        }
+
+    private var onCheckName: String? = null
+    var onCheck: Method? = null
+
+    init {
+        if (attrs != null) {
+            val a = context.obtainStyledAttributes(attrs, R.styleable.ModifiedChip)
+            checked = a.getBoolean(R.styleable.ModifiedChip_checked, false)
+            onCheckName = a.getString(R.styleable.ModifiedChip_onCheck)
+            Log.println(Log.DEBUG, "Chip", "OnCheckName: $onCheckName")
+            try {
+                onCheck = context.javaClass.getMethod(onCheckName, this::class.java)
+            } catch (e: NoSuchMethodException) {
+                Log.println(Log.ERROR, "Chip", e.toString())
+                throw e
+            }
+            a.recycle()
+        }
+
+        setOnClickListener {
+            onClick()
+        }
+        setBackgroundResource(R.drawable.chip_unselected)
+        textAlignment = TEXT_ALIGNMENT_CENTER
+        gravity = android.view.Gravity.CENTER
+    }
+
+    private fun onClick() {
+        checked = !checked
+        onCheck?.invoke(context, this)
+    }
+}
